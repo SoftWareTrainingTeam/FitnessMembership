dbd83c2b8b1bcd9804290f342b54c581e0966b69
eat to 160 pounds <2915390277@qq.com>
2022-05-18 12:29:11 +0800

    页面做了些修改

--- fitness-management-system/src/components/Footer/index.tsx
+++ fitness-management-system/src/components/Footer/index.tsx
@@ -8,26 +8,6 @@ const Footer: React.FC = () => {
   return (
     <DefaultFooter
       copyright={`${currentYear} ${defaultMessage}`}
-      links={[
-        {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
-        {
-          key: 'github',
-          title: "GitHub",
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
-      ]}
     />
   );
 };
